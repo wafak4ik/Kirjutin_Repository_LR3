7b182b2ce5b75553d0def929e357c0ef3bfb7edf
wafak4ik <nikita.kiriutin@gmail.com>
2025-03-25 11:33:54 +0300

    final fixes

--- Kirjutin_LR3_project.cpp
+++ Kirjutin_LR3_project.cpp
@@ -4,20 +4,6 @@
 
 using namespace std;
 
-/*- ввести вещественное число x
-- ввести вещественное число y
-- ввести вещественное число z
-- найти целую часть суммы этих 3-х чисел
-- найти дробную часть среднего геометрического модулей этих 3-х чисел */
-
-/* - enter the real number x
-- enter the real number y
-- enter a real number z
-- find the integer part of the sum of these 3 numbers
-- find the fractional part of the geometric mean of the modules of these 3 numbers */
-
-double x, y, z;
-
 int prov(int& num1)
 {
 	while (true)
@@ -32,7 +18,8 @@ int prov(int& num1)
 		}
 		else break;
 	}
-	cout << "You entered: " << num1 << endl;
+	cout << " " << endl;
+    cout << "You entered: " << num1 << endl;
 	cout << " " << endl;
 	return num1;
 }
@@ -51,14 +38,15 @@ int prov(double& num1)
 		}
 		else break;
 	}
-	cout << "You entered: " << num1 << endl;
+	cout << " " << endl;
+    cout << "You entered: " << num1 << endl;
 	cout << " " << endl;
 	return num1;
 }
 
 double task1(double& x)
 {
-    cout << "Enter a real number x (No more than 15 characters after the dot)" << endl;
+    cout << "Enter a real number x (No more than 15 characters after the dot):" << endl;
     cout << " " << endl;
     prov(x);
     return x;
@@ -66,7 +54,7 @@ double task1(double& x)
 
 double task2(double& y)
 {
-    cout << "Enter a real number y (No more than 15 characters after the dot)" << endl;
+    cout << "Enter a real number y (No more than 15 characters after the dot):" << endl;
     cout << " " << endl;
     prov(y);
     return y;
@@ -74,19 +62,21 @@ double task2(double& y)
 
 double task3(double& z)
 {
-    cout << "Enter a real number z (No more than 15 characters after the dot)" << endl;
+    cout << "Enter a real number z (No more than 15 characters after the dot):" << endl;
     cout << " " << endl;
     prov(z);
     return z;
 }
 
-int task4()
+int task4(double x, double y, double z)
 {
+    cout << " " << endl;
     int sum = floor(x + y + z);
+    cout << " " << endl;
     return sum; 
 }
 
-double task5()
+double task5(double x, double y, double z)
 {
     double moduleX = fabs(x);
     double moduleY = fabs(y);
@@ -97,29 +87,35 @@ double task5()
 
     double drob = geometricMean - floor(geometricMean);
 
+    cout << " " << endl;
     return drob;
 }
 
 int main() 
 {
     int h;
-    double x, y, z;
-
-    cout << "Welcome. Please, enter the number of task you wanna run or enter 0, if you wanna exit the program." << endl;
-    cout << "1 - enter the real number x" << endl;
-    cout << "2 - enter the real number y" << endl;
-    cout << "3 - enter a real number z" << endl;
-    cout << "4 - find the integer part of the sum of these 3 numbers" << endl;
-    cout << "5 - find the fractional part of the geometric mean of the modules of these 3 numbers" << endl;
-    prov(h);
+    double x = 0, y = 0, z = 0;
 
     while(true)
     {
+        cout << "=====================================================================================================" << endl;
+        cout << "Welcome. Please, enter the number of task you wanna run or enter 0, if you wanna exit the program." << endl;
+        cout << "1 - enter the real number x" << endl;
+        cout << "2 - enter the real number y" << endl;
+        cout << "3 - enter a real number z" << endl;
+        cout << "4 - find the integer part of the sum of these 3 numbers" << endl;
+        cout << "5 - find the fractional part of the geometric mean of the modules of these 3 numbers" << endl;
+        cout << "=====================================================================================================" << endl;
+        
+        cout << "Your choice: ";
+        prov(h);
+
         switch(h)
         {
             case 0:
                 cout << "Shutting down the program. Goodbye!" << endl;
-                break;
+                cout << " " << endl;
+                return 0;
             case 1:
                 task1(x);
                 break;
@@ -130,15 +126,16 @@ int main()
                 task3(z);
                 break;
             case 4:
-                task4();
+                cout << "Integer part of the sum: " << task4(x, y, z) << endl;
+                cout << " " << endl;
                 break;
             case 5:
-                task5();
+                cout << "Fractional part of the geometric mean: " << task5(x, y, z) << endl;
+                cout << " " << endl;
                 break;
             default:
-                cout << "There are no options that need this button" << endl;
+                cout << "Invalid option. Please enter a valid number." << endl;
                 cout << " " << endl;
-                cin.clear();
                 break;
         }
     }
